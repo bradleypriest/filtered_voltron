5b29a846c787fd5998380ff4909da602d1b5c6a9
Bradley Priest <bradleypriest@gmail.com>
2023-03-27 17:04:13 -0700

    Minor cleanup

--- convert.rb
+++ convert.rb
@@ -2,14 +2,19 @@
 
 require 'http'
 
+# Very naive implementation, aiming for minimal false negatives for now
+# Currently:
+#   - If it has a tags section
+#   - If the tag is in any part of the text
+def tagged_with?(text, tag)
+  !text.include?("tags:") || text.include?(tag)
+end
+
 def filter_wishlist(input_url, output_path, tag='pve')
   wishlist_text = HTTP.get(input_url).body.to_s
 
   filtered_wishlist = wishlist_text.split(/\n\n\/\//).select do |chunk|
-    downcased = chunk.downcase
-    !downcased.include?("dimwishlist") ||
-      !downcased.include?("tags:") ||
-      downcased.include?(tag)
+    !chunk.include?("dimwishlist") || tagged_with?(chunk.downcase, tag)
   end.join("\n\n//")
 
   File.write("dist/#{output_path}", filtered_wishlist)
